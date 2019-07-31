5fde64dd323fd6687b218bb6aa6fcec1787a8e01
plusls <plusls@qq.com>
2019-07-31 20:26:09 +0800

    clean data

--- 2019/ciscn-final-2019/pwn/c08/update/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-chmod 777 pwn
-mv -f pwn /pwn/pwn
-chmod 777 /pwn/pwn
--- 2019/ciscn-final-2019/pwn/c16/update/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-chmod 777 pwn
-chmod 777 source
-mv -f pwn /home/ctf/source
-mv -f pwn /pwn/pwn
-chmod 777 /pwn/pwn
-chmod 777 /home/ctf/source
